d846dc25d36d25bcfb5b9143be90557cf55f7564
Jagan0901 <Jaganath0901@gmail.com>
2023-03-27 21:26:42 +0530

    init

--- routes/gold.js
+++ routes/gold.js
@@ -15,7 +15,7 @@ router.post("/post", async(req,res) => {
     const goldPrice = +price;
     const create = await createData(date,goldPrice);
 
-    res.send({message: "Created Successfully"});
+    res.send(create);
 });
 
 router.get("/get/today", async(req,res) => {
